3cf36c7e49a30a0dee7aa5139695ebce0c45fa69
Sam <samuel.waters@gmail.com>
2022-03-15 16:07:47 -0600

    this assignment doesnt work

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,8 +2,15 @@ import React from "react";
 
 function NavBar() {
   const links = ["home", "about", "projects"];
-
-  return <nav>{/* display an <a> tag for each link here */}</nav>;
-}
+  const anchor = links.map((link) => { 
+  return(
+    <a href={"#"+link} key={link}> {link} </a> 
+  )
+  })
+  
+  return (
+    <nav>{anchor}</nav>
+  )
+};
 
 export default NavBar;
